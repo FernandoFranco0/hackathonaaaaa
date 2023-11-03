6f3feb5ca68ddca951c9c5647c4be07136fff479
Alexandre Campos <Alexandre.Campos@AB-inbev.com>
2023-11-03 10:50:29 -0300

    adding padding

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -2,17 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hackathon/login_page.dart';
 
-class HomePage extends StatefulWidget{
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
   State<StatefulWidget> createState() {
     return HomePageState();
   }
-
 }
 
-class HomePageState extends State<HomePage>{
+class HomePageState extends State<HomePage> {
   bool anim = false;
 
   @override
@@ -23,19 +22,17 @@ class HomePageState extends State<HomePage>{
 
   @override
   Widget build(BuildContext context) {
-    return(
-      Scaffold(
-        body: Container(
+    return (Scaffold(
+      body: Container(
           decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [
-                Color(0xFFc4bbaf),
-                Color(0xFFa5978b),
-              ],
-            )
-          ),
+              gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Color(0xFFc4bbaf),
+              Color(0xFFa5978b),
+            ],
+          )),
           width: double.infinity,
           height: double.infinity,
           child: Column(
@@ -44,40 +41,56 @@ class HomePageState extends State<HomePage>{
               Center(
                 child: Text(
                   'Ewá',
-                  style: GoogleFonts.kristi(fontSize: 96, color: Colors.black, fontWeight: FontWeight.w400),
+                  style: GoogleFonts.kristi(
+                      fontSize: 96,
+                      color: Colors.black,
+                      fontWeight: FontWeight.w400),
                   textDirection: TextDirection.ltr,
                 ),
               ),
-               SizedBox(height: anim ? 150 : 1),
-              if (anim) SizedBox(
-                width: 270,
-                height: 60,
-                child: ElevatedButton(
-                  onPressed: () { 
-                    Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => const LoginPage()));
-                   }, 
-                  style: ElevatedButton.styleFrom(
-                    elevation: 0.0,
-                    shadowColor: Colors.transparent,
-                    backgroundColor: const Color(0xFFc4bbaf),
-                    shape: const StadiumBorder()
-                
-                  ),
-                  child: const Text("Iniciar",style: TextStyle(color: Colors.black, fontSize: 30, fontWeight: FontWeight.w500),),
-                ),
-              )
+              AnimatedSwitcher(
+                duration: const Duration(milliseconds: 300),
+                child: anim
+                    ? Column(
+                        children: [
+                          const SizedBox(height: 150),
+                          SizedBox(
+                            width: 270,
+                            height: 60,
+                            child: ElevatedButton(
+                              onPressed: () {
+                                Navigator.of(context).pushReplacement(
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            const LoginPage()));
+                              },
+                              style: ElevatedButton.styleFrom(
+                                  elevation: 0.0,
+                                  shadowColor: Colors.transparent,
+                                  backgroundColor: const Color(0xFFc4bbaf),
+                                  shape: const StadiumBorder()),
+                              child: const Text(
+                                "Iniciar",
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 30,
+                                    fontWeight: FontWeight.w500),
+                              ),
+                            ),
+                          ),
+                        ],
+                      )
+                    : const SizedBox.shrink(),
+              ),
             ],
-          )
-        ),
-      )
-    );
+          )),
+    ));
   }
-  
+
   Future startAnimation() async {
     await Future.delayed(const Duration(milliseconds: 1500));
     setState(() {
       anim = true;
     });
-
   }
 }
--- lib/register_inst_page.dart
+++ lib/register_inst_page.dart
@@ -12,90 +12,125 @@ class _InstRegisterPageState extends State<InstRegisterPage> {
   int button = 0;
   @override
   Widget build(BuildContext context) {
-    return  Material(
+    return Material(
       color: const Color(0xFFA5978B),
-      child: ListView(
-        children: [Column(
+      child: ListView(children: [
+        Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const SizedBox(height: 50,),
-            const Text("Faça parte",style: TextStyle(color: Colors.black, fontSize: 30, fontWeight: FontWeight.w500),),
-            const SizedBox(height: 30,),
+            const SizedBox(
+              height: 50,
+            ),
+            const Text(
+              "Faça parte",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 30,
+                  fontWeight: FontWeight.w500),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
             SizedBox(
               width: 300,
               child: TextField(
                 decoration: InputDecoration(
                   filled: true,
                   fillColor: const Color(0xFFC4BBAF),
-                  labelText: "Nome da instituição", 
+                  labelText: "Nome da instituição",
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(10.0),
                   ),
                 ),
               ),
             ),
-            const SizedBox(height: 8,),
-            SizedBox(
-              width: 300,
-              child: Row(
-                children: [
-                  ElevatedButton(
-                    onPressed: () { 
-                      setState(() {
-                        button = 1;
-                      });
-                     }, 
-                    style: ElevatedButton.styleFrom(
-                      elevation: 0.0,
-                      shadowColor: Colors.transparent,
-                      backgroundColor: button == 1 ? const Color(0xFF0000ff) : const Color(0xFFC4BBAF),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30),
-                      )
-                  
-                    ),
-                    child: const Text("Museu",style: TextStyle(color: Colors.black, fontSize: 18, fontWeight: FontWeight.w500),),
-                  ),
-                  const SizedBox(width: 4,),
-                  ElevatedButton(
-                    onPressed: () { 
-                      setState(() {
-                        button = 2;
-                      });
-                     }, 
-                    style: ElevatedButton.styleFrom(
-                      elevation: 0.0,
-                      shadowColor: Colors.transparent,
-                      backgroundColor: button == 2 ? const Color(0xFF0000ff) : const Color(0xFFC4BBAF),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30),
-                      )
-                  
-                    ),
-                    child: const Text("Galeria",style: TextStyle(color: Colors.black, fontSize: 18, fontWeight: FontWeight.w500),),
-                  ),
-                  const SizedBox(width: 4,),
-                  ElevatedButton(
-                    onPressed: () { 
-                      setState(() {
-                        button = 3;
-                      });
-                     }, 
-                    style: ElevatedButton.styleFrom(
-                      elevation: 0.0,
-                      shadowColor: Colors.transparent,
-                      backgroundColor: button == 3 ? const Color(0xFF0000ff) : const Color(0xFFC4BBAF),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30),
-                      )
-                  
-                    ),
-                    child: const Text("Exposição",style: TextStyle(color: Colors.black, fontSize: 18, fontWeight: FontWeight.w500),),
-                  ),
-                ],
-              )
+            const SizedBox(
+              height: 8,
             ),
-            const SizedBox(height: 8,),
+            SizedBox(
+                child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      button = 1;
+                    });
+                  },
+                  style: ElevatedButton.styleFrom(
+                      elevation: 0.0,
+                      shadowColor: Colors.transparent,
+                      backgroundColor: button == 1
+                          ? const Color(0xFF0000ff)
+                          : const Color(0xFFC4BBAF),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      )),
+                  child: const Text(
+                    "Museu",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500),
+                  ),
+                ),
+                const SizedBox(
+                  width: 4,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      button = 2;
+                    });
+                  },
+                  style: ElevatedButton.styleFrom(
+                      elevation: 0.0,
+                      shadowColor: Colors.transparent,
+                      backgroundColor: button == 2
+                          ? const Color(0xFF0000ff)
+                          : const Color(0xFFC4BBAF),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      )),
+                  child: const Text(
+                    "Galeria",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500),
+                  ),
+                ),
+                const SizedBox(
+                  width: 4,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      button = 3;
+                    });
+                  },
+                  style: ElevatedButton.styleFrom(
+                      elevation: 0.0,
+                      shadowColor: Colors.transparent,
+                      backgroundColor: button == 3
+                          ? const Color(0xFF0000ff)
+                          : const Color(0xFFC4BBAF),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      )),
+                  child: const Text(
+                    "Exposição",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500),
+                  ),
+                ),
+              ],
+            )),
+            const SizedBox(
+              height: 8,
+            ),
             SizedBox(
               width: 300,
               child: TextField(
@@ -104,120 +139,132 @@ class _InstRegisterPageState extends State<InstRegisterPage> {
                 decoration: InputDecoration(
                   filled: true,
                   fillColor: const Color(0xFFC4BBAF),
-                  labelText: "Descrição", 
+                  labelText: "Descrição",
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(10.0),
                   ),
                 ),
               ),
             ),
-            const SizedBox(height: 8,),
+            const SizedBox(
+              height: 8,
+            ),
             SizedBox(
               width: 300,
               child: TextField(
                 decoration: InputDecoration(
                   filled: true,
                   fillColor: const Color(0xFFC4BBAF),
-                  labelText: "CEP", 
+                  labelText: "CEP",
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(10.0),
                   ),
                 ),
               ),
             ),
-            const SizedBox(height: 8,),
+            const SizedBox(
+              height: 8,
+            ),
             SizedBox(
               width: 300,
               child: TextField(
                 decoration: InputDecoration(
                   filled: true,
                   fillColor: const Color(0xFFC4BBAF),
-                  labelText: "Logradouro *", 
+                  labelText: "Logradouro *",
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(10.0),
                   ),
                 ),
               ),
             ),
-            const SizedBox(height: 8,),
+            const SizedBox(
+              height: 8,
+            ),
             SizedBox(
               width: 300,
               child: TextField(
                 decoration: InputDecoration(
                   filled: true,
                   fillColor: const Color(0xFFC4BBAF),
-                  labelText: "Número", 
+                  labelText: "Número",
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(10.0),
                   ),
                 ),
               ),
             ),
-            const SizedBox(height: 8,),
+            const SizedBox(
+              height: 8,
+            ),
             SizedBox(
               width: 300,
               child: TextField(
                 decoration: InputDecoration(
                   filled: true,
                   fillColor: const Color(0xFFC4BBAF),
-                  labelText: "Complemento", 
+                  labelText: "Complemento",
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(10.0),
                   ),
                 ),
               ),
             ),
-            const SizedBox(height: 8,),
+            const SizedBox(
+              height: 8,
+            ),
             SizedBox(
               width: 300,
               child: TextField(
                 decoration: InputDecoration(
                   filled: true,
                   fillColor: const Color(0xFFC4BBAF),
-                  labelText: "Cidade *", 
+                  labelText: "Cidade *",
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(10.0),
                   ),
                 ),
               ),
             ),
-            const SizedBox(height: 8,),
+            const SizedBox(
+              height: 8,
+            ),
             SizedBox(
               width: 300,
               child: TextField(
                 decoration: InputDecoration(
                   filled: true,
                   fillColor: const Color(0xFFC4BBAF),
-                  labelText: "Estado *", 
+                  labelText: "Estado *",
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(10.0),
                   ),
                 ),
               ),
             ),
-            const SizedBox(height: 15,),
-            SizedBox(
-                  width: 300,
-                  height: 40,
-                  child: ElevatedButton(
-                    onPressed: () { Navigator.of(context).push(MaterialPageRoute(builder: (context) => const SecondePart())); },  
-                    style: ElevatedButton.styleFrom(
-                      elevation: 0.0,
-                      shadowColor: Colors.transparent,
-                      backgroundColor: const Color(0xFFc4bbaf),
-                      shape: const CircleBorder()
-                  
-                    ),
-                    child: const Text("=>",style: TextStyle(color: Colors.black, fontSize: 30, fontWeight: FontWeight.w500),),
-                  ),
-                )
+            const SizedBox(
+              height: 15,
+            ),
+            CircleAvatar(
+              radius: 30,
+              backgroundColor: const Color(0xFFc4bbaf),
+              child: IconButton(
+                icon: const Icon(Icons.arrow_forward),
+                color: Colors.black,
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                        builder: (context) => const SecondePart()),
+                  );
+                },
+              ),
+            ),
           ],
         ),
       ]),
     );
   }
 }
-
 
 class SecondePart extends StatefulWidget {
   const SecondePart({super.key});
@@ -240,57 +287,60 @@ class _SecondePartState extends State<SecondePart> {
               decoration: InputDecoration(
                 filled: true,
                 fillColor: const Color(0xFFC4BBAF),
-                labelText: "Nome do responsavel *", 
+                labelText: "Nome do responsavel *",
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(10.0),
                 ),
               ),
             ),
           ),
-          const SizedBox(height: 15,),
+          const SizedBox(
+            height: 15,
+          ),
           SizedBox(
             width: 300,
             child: TextField(
               decoration: InputDecoration(
                 filled: true,
                 fillColor: const Color(0xFFC4BBAF),
-                labelText: "Email *", 
+                labelText: "Email *",
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(10.0),
                 ),
               ),
             ),
           ),
-          const SizedBox(height: 15,),
+          const SizedBox(
+            height: 15,
+          ),
           SizedBox(
             width: 300,
             child: TextField(
               decoration: InputDecoration(
                 filled: true,
                 fillColor: const Color(0xFFC4BBAF),
-                labelText: "Telefone para contato", 
+                labelText: "Telefone para contato",
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(10.0),
                 ),
               ),
             ),
           ),
-          const SizedBox(height: 50,),
-          SizedBox(
-            width: 300,
-            height: 40,
-            child: ElevatedButton(
-              onPressed: () { Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => const RegisterComplete())); }, 
-              style: ElevatedButton.styleFrom(
-                elevation: 0.0,
-                shadowColor: Colors.transparent,
-                backgroundColor: const Color(0xFFc4bbaf),
-                shape: const CircleBorder()
-            
-              ),
-              child: const Text("=>",style: TextStyle(color: Colors.black, fontSize: 30, fontWeight: FontWeight.w500),),
+          const SizedBox(
+            height: 50,
+          ),
+          CircleAvatar(
+            radius: 30,
+            backgroundColor: const Color(0xFFc4bbaf),
+            child: IconButton(
+              icon: const Icon(Icons.arrow_forward),
+              color: Colors.black,
+              onPressed: () {
+                Navigator.of(context).pushReplacement(MaterialPageRoute(
+                    builder: (context) => const RegisterComplete()));
+              },
             ),
-          )
+          ),
         ],
       ),
     );
